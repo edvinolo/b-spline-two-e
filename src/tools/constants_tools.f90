5103b6d717e69802a919d813b535943290dddb48
edvino <edvin.olofsson@matfys.lth.se>
2025-04-22 14:04:59 +0200

    Add some more constants

--- src/tools/constants_tools.f90
+++ src/tools/constants_tools.f90
@@ -8,10 +8,14 @@ public
     real(dp), parameter :: time_au = 2.4188843265857e-17_dp
     real(dp), parameter :: fs_to_au = 1e-15_dp/time_au
     real(dp), parameter :: alpha = 0.007297352562787135_dp
-    real(dp), parameter :: c = 299792458_dp
+    real(dp), parameter :: c = 299792458.0_dp
     real(dp), parameter :: e = 1.60217662e-19_dp
     real(dp), parameter :: hbar = 1.0545718e-34_dp
     real(dp), parameter :: I_au = 3.51e16_dp
+    real(dp), parameter :: pi = 4.0_dp*atan2(1.0_dp,1.0_dp)
+    real(dp), parameter :: sqrt_2 = sqrt(2.0_dp)
+    real(dp), parameter :: inv_sqrt_2 = 1.0_dp/sqrt_2
+    complex(dp), parameter :: i_ = (0.0_dp,1.0_dp)
 
 contains
 
