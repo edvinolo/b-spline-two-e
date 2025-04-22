fd54121d2ce75767b6d432d4a3fc2ff391d3613f
edvino <edvin.olofsson@matfys.lth.se>
2025-04-22 13:53:05 +0200

    Add kinds and constants modules
    Add a module to store kind parameters. Will perform a refactor to type
    declarations to use kind parameters in the future. Add a module to store
    constants. It also contains some functions for unit conversions.

---
+++ src/tools/constants_tools.f90
@@ -0,0 +1,32 @@
+module constants_tools
+use kind_tools
+implicit none
+
+public
+    real(dp), parameter :: length_au = 5.291772109e-11_dp
+    real(dp), parameter :: au_to_eV= 27.211386245988_dp
+    real(dp), parameter :: time_au = 2.4188843265857e-17_dp
+    real(dp), parameter :: fs_to_au = 1e-15_dp/time_au
+    real(dp), parameter :: alpha = 0.007297352562787135_dp
+    real(dp), parameter :: c = 299792458_dp
+    real(dp), parameter :: e = 1.60217662e-19_dp
+    real(dp), parameter :: hbar = 1.0545718e-34_dp
+    real(dp), parameter :: I_au = 3.51e16_dp
+
+contains
+
+    pure function I_W_cm2(E_0) result(res)
+        real(dp), intent(in) :: E_0
+        real(dp) :: res
+
+        res = E_0**2*I_au
+    end function I_W_cm2
+
+    pure function E_0_au(I) result(res)
+        real(dp), intent(in) :: I
+        real(dp) :: res
+
+        res = sqrt(I/I_au)
+    end function E_0_au
+
+end module constants_tools
