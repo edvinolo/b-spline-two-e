9b6c5a8f9d86c1914a7bcc64b5a840281904ca48
edvino <edvin.olofsson@matfys.lth.se>
2025-10-23 16:03:15 +0200

    Allow for full matrix in PARDISO
    Update the PARDISO module to allow for general complex matrices. Add
    MACRO DEBUG_PARDISO, that can be set via CMake option USE_DEBUG_PARDISO.
    Enables printing of stats and activates the matrix checker. Update the
    Block Jacobi Preconditioner to allow for full matrices. Some reordering
    and renaming of variables in main_quasienergies.

--- src/tools/PARDISO_tools.f90
+++ src/tools/PARDISO_tools.f90
@@ -8,6 +8,7 @@ module PARDISO_tools
         type(MKL_PARDISO_HANDLE), dimension(:), allocatable  :: pt !Pointers used by PARDISO
         integer, dimension(:), allocatable :: iparm !PARDISO parameter array
         integer :: maxfct, mnum, mtype, phase, n, nrhs, error, msglvl, nnz !PARDISO integer parameters
+        logical :: full, first
         integer, dimension(1) :: idum !dummy integer array
         complex(dp), dimension(1) :: ddum !dummy complex array
 
@@ -31,10 +32,11 @@ module PARDISO_tools
     end type PARDISO_solver_sp
 
 contains
-    subroutine PARDISO_set_params(this,n,nnz,use_sp)
+    subroutine PARDISO_set_params(this,n,nnz,full,use_sp)
         class(PARDISO_solver), intent(inout) :: this
         integer, intent(in) :: n
         integer, intent(in) :: nnz
+        logical, intent(in) :: full
         logical, intent(in) :: use_sp
 
         integer :: i !loop index
@@ -43,10 +45,19 @@ contains
         this%n = n
         this%nnz = nnz
         this%nrhs = 1 !Hard code this for now, could change later
+        this%full = full
 
         this%error  = 0 !initialize error flag
+#if defined(DEBUG_PARDISO)
+        this%msglvl = 1 !print statistical information
+#else
         this%msglvl = 0 !print no statistical information
-        this%mtype  = 6 !complex symmetric
+#endif
+        if (this%full) then
+            this%mtype = 13 ! complex general
+        else
+            this%mtype  = 6 ! complex symmetric
+        end if
         this%maxfct = 1 !Maximum number of factors
         this%mnum = 1 !Number of matrix to solve
 
@@ -69,30 +80,49 @@ contains
         !Please see the intel MKL online documentation for PARDISO for the complete meaning of all params
         this%iparm(1) = 1 !Do not use defaults for all iparm
         this%iparm(2) = 3 !Parallel fill in reordering
-        this%iparm(10) = 8 !Perturbation of small pivots by 1e-8
-        this%iparm(11) = 0 !1: Enable scaling vectors
-        this%iparm(13) = 0 !1: Enable weighted matching
+        if (this%full) then
+            this%iparm(10) = 13 !Perturbation of small pivots by 1e-13
+        else
+            this%iparm(10) = 8 !Perturbation of small pivots by 1e-8
+        endif
+        if (this%full) then
+            this%iparm(11) = 1 !1: Enable scaling vectors, must supply numerical values during phase 11
+            this%iparm(13) = 1 !1: Enable weighted matching, must supply numerical values during phase 11
+        else
+            this%iparm(11) = 0 !1: Enable scaling vectors
+            this%iparm(13) = 0 !1: Enable weighted matching
+        end if
         this%iparm(21) = 1 !Enable Bunch-Kaufman pivoting for complex symmetric matrix
-        this%iparm(24) = 10 !Change to 10 for two-level factorization (must disable param 11 and 13 if used)
+        if (this%full) then
+            this%iparm(24) = 0 ! Use classical factorization algorithm, must be used if 11 and 13 are used
+        else
+            this%iparm(24) = 10 !Change to 10 for two-level factorization (must disable param 11 and 13 if used)
+        end if
+#if defined(DEBUG_PARDISO)
+        this%iparm(27) = 1 !Enable matrix checker, could be useful for debugging, but probably disable later
+#else
         this%iparm(27) = 0 !Enable matrix checker, could be useful for debugging, but probably disable later
-
+#endif
         if (use_sp) then
             this%iparm(28) = 1 ! Use single precision
         end if
 
+        this%first = .true.
+
     end subroutine PARDISO_set_params
 
-    subroutine PARDISO_setup(this,n,nnz,a,ia,ja)
+    subroutine PARDISO_setup(this,n,nnz,a,ia,ja,full)
         class(PARDISO_solver), intent(inout) :: this
         integer, intent(in) :: n
         integer, intent(in) :: nnz
         complex(dp), dimension(nnz), intent(in) :: a(nnz)
         integer, dimension(n), intent(in) :: ia(n+1)
         integer, dimension(nnz), intent(in) :: ja(nnz)
+        logical, intent(in) :: full
 
         logical, parameter :: use_sp = .false.
 
-        call PARDISO_set_params(this,n,nnz,use_sp)
+        call PARDISO_set_params(this,n,nnz,full,use_sp)
 
         write(6,*) ""
         write(6,*) "Performing PARDISO setup..."
@@ -106,17 +136,18 @@ contains
         write(6,*) "Done!"
     end subroutine PARDISO_setup
 
-    subroutine PARDISO_setup_sp(this,n,nnz,a,ia,ja)
+    subroutine PARDISO_setup_sp(this,n,nnz,a,ia,ja,full)
         class(PARDISO_solver_sp), intent(inout) :: this
         integer, intent(in) :: n
         integer, intent(in) :: nnz
         complex(dp), dimension(nnz), intent(in) :: a(nnz)
         integer, dimension(n), intent(in) :: ia(n+1)
         integer, dimension(nnz), intent(in) :: ja(nnz)
+        logical, intent(in) :: full
 
         logical, parameter :: use_sp = .true.
 
-        call PARDISO_set_params(this,n,nnz,use_sp)
+        call PARDISO_set_params(this,n,nnz,full,use_sp)
 
         allocate(this%a(nnz), this%x(n),this%b(n))
 
@@ -142,7 +173,12 @@ contains
 
         write(6,*) ""
         write(6,*) "Performing PARDISO factorization..."
-        this%phase = 22 !Numerical factorization
+        if (this%full.and.(.not.this%first)) then
+            this%phase = 12 !Analysis and factorization
+        else
+            this%phase = 22 !Numerical factorization
+            this%first = .false.
+        end if
         call pardiso_64 (this%pt, this%maxfct, this%mnum, this%mtype, this%phase, this%n, a, ia, ja, &
               this%idum, this%nrhs, this%iparm, this%msglvl, this%ddum, this%ddum, this%error)
         write(6,*) "Done!"
@@ -158,7 +194,12 @@ contains
 
         write(6,*) ""
         write(6,*) "Performing PARDISO factorization..."
-        this%phase = 22 !Numerical factorization
+        if (this%full.and.(.not.this%first)) then
+            this%phase = 12 !Analysis and factorization
+        else
+            this%phase = 22 !Numerical factorization
+            this%first = .false.
+        end if
         call pardiso_64 (this%pt, this%maxfct, this%mnum, this%mtype, this%phase, this%n, this%a, ia, ja, &
               this%idum, this%nrhs, this%iparm, this%msglvl, this%sdum, this%sdum, this%error)
         write(6,*) "Done!"
