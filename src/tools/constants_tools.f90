f2b86ebbb2568b91ab996936de51616c86c22be9
edvino <edvin.olofsson@matfys.lth.se>
2026-01-15 04:05:31 +0100

    Rename speed of light from c to C_SI

--- src/tools/constants_tools.f90
+++ src/tools/constants_tools.f90
@@ -8,7 +8,7 @@ public
     real(dp), parameter :: time_au = 2.4188843265857e-17_dp
     real(dp), parameter :: fs_to_au = 1e-15_dp/time_au
     real(dp), parameter :: alpha = 0.007297352562787135_dp
-    real(dp), parameter :: c = 299792458.0_dp
+    real(dp), parameter :: c_SI = 299792458.0_dp
     real(dp), parameter :: e = 1.60217662e-19_dp
     real(dp), parameter :: hbar = 1.0545718e-34_dp
     real(dp), parameter :: I_au = 3.51e16_dp
